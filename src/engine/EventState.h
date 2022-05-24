80d354e0ecbce8194bc9fb3789adb4e3a061bba5
p014 <pc165@pm.me>
2022-05-24 10:16:01 +0200

    Refactor

--- src/engine/EventState.h
+++ src/engine/EventState.h
@@ -1,70 +1,105 @@
-//#ifndef TFG_EVENTSTATE_H
-//#define TFG_EVENTSTATE_H
-//
-//#include <vector>
-//#include <ctime>
-//#include "OpenGL.h"
-//#include "Logger.h"
-//
-//class EventState {
-//private:
-//    const int KEYBOARD_SIZE{GLFW_KEY_LAST};
-//    const int MOUSE_KEY_SIZE{GLFW_MOUSE_BUTTON_LAST};
-//    const int NONE{-1};
-//    std::vector<int> stateKeys;
-//    std::vector<int> activeKeys;
-//
-//    std::vector<int> stateMouseKeys;
-//    std::vector<int> activeMouseKeys;
-//
-//public:
-//
-//    EventState() : stateKeys(KEYBOARD_SIZE, NONE), activeKeys(KEYBOARD_SIZE, NONE),
-//                   stateMouseKeys(MOUSE_KEY_SIZE, NONE), activeMouseKeys(MOUSE_KEY_SIZE, NONE) {
-//    }
-//
-//    void reset() {
-//        for (auto &i: stateKeys)
-//            i = NONE;
-//        for (auto &i: stateMouseKeys)
-//            i = NONE;
-//    }
-//
-//
-//    void mouseHandler(int button, int action) {
-//        activeMouseKeys[button] = action != GLFW_PRESS;
-//        stateMouseKeys[button] = action;
-//    }
-//
-//    void keyboardHandler(int key, int action) {
-//        activeKeys[key] = action != GLFW_PRESS;
-//        stateKeys[key] = action;
-//    }
-//
-//    bool keyDown(int key) const {
-//        LOG_DEBUG("{} {}", activeKeys[key], stateKeys[key]);
-//        return activeKeys[key];
-//    }
-//
-//    bool keyPressed(int key) const {
-//        return stateKeys[key] == GLFW_PRESS;
-//    }
-//
-//    bool keyReleased(int key) const {
-//        return stateKeys[key] == GLFW_RELEASE;
-//    }
-//
-//    bool mouseButtonDown(int button) const {
-//        return activeMouseKeys[button];
-//    }
-//
-//    bool mouseButtonPressed(int button) const {
-//        return stateMouseKeys[button] == GLFW_RELEASE;
-//    }
-//
-//    bool mouseButtonReleased(int button) const {
-//        return stateMouseKeys[button] == GLFW_RELEASE;
-//    }
-//};
-//
-//#endif //TFG_EVENTSTATE_H
+#ifndef TFG_EVENTSTATE_H
+#define TFG_EVENTSTATE_H
+
+#include <utility>
+#include <vector>
+#include <ctime>
+#include <GLFW/glfw3.h>
+#include "Logger.h"
+#include "Event.h"
+
+class EventState {
+public:
+    enum class InputType {
+        Keyboard = 0, Mouse
+    };
+
+    struct CallBack {
+        InputType inputType{-1};
+        int key = -1;
+        int press_release_repeat = -1;
+        std::function<void()> callback = []() {
+            LOG_DEBUG("No callback set!");
+        };
+    };
+
+    EventState() : stateKeys(KEYBOARD_SIZE, NONE), activeKeys(KEYBOARD_SIZE, NONE),
+                   stateMouseKeys(MOUSE_KEY_SIZE, NONE), activeMouseKeys(MOUSE_KEY_SIZE, NONE) {
+    }
+
+    void subscribe(InputType type, int key, int press_release_repeat, std::function<void()> &callback) {
+        CallBack d;
+        d.inputType = type;
+        d.key = key;
+        d.press_release_repeat = press_release_repeat;
+        d.callback = std::move(callback);
+        callbackList.emplace_back(d);
+    }
+
+    void dispatch() {
+        for (auto &i: callbackList) {
+            switch (i.inputType) {
+                case InputType::Keyboard: {
+                    if (stateKeys[i.key] == i.press_release_repeat) {
+                        i.callback();
+                        break;
+                    }
+                }
+                case InputType::Mouse: {
+                    if (stateMouseKeys[i.key] == i.press_release_repeat) {
+                        i.callback();
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    void reset() {
+        for (auto &i: stateKeys)
+            i = NONE;
+        for (auto &i: stateMouseKeys)
+            i = NONE;
+    }
+
+    bool onEvent(const Event &event) {
+        switch (event.type) {
+            case Key: {
+                auto key = dynamic_cast<const KeyEvent *>(&event);
+                activeKeys[key->key] = key->press_release_repeat == 0; // active if key is pressed
+                stateKeys[key->key] = key->press_release_repeat;
+                break;
+            }
+            case MouseButton: {
+                auto mouse = dynamic_cast<const MouseButtonEvent *>(&event);
+                activeMouseKeys[mouse->button] = mouse->press_release == 0; // active if mouse button is pressed
+                stateMouseKeys[mouse->button] = mouse->press_release;
+                break;
+            }
+
+            case MouseMoved: {
+                auto mouse = dynamic_cast<const MouseButtonEvent *>(&event);
+                activeMouseKeys[mouse->button] = mouse->press_release == 0; // active if mouse button is pressed
+                stateMouseKeys[mouse->button] = mouse->press_release;
+                break;
+            }
+            default:
+                break;
+        }
+        return false;
+    }
+
+private:
+    const int KEYBOARD_SIZE{GLFW_KEY_LAST};
+    const int MOUSE_KEY_SIZE{GLFW_MOUSE_BUTTON_LAST};
+    const int NONE{-1};
+    std::vector<int> stateKeys;
+    std::vector<int> activeKeys;
+    std::vector<int> stateMouseKeys;
+    std::vector<int> activeMouseKeys;
+    float mouseX{0};
+    float mouseY{0};
+    std::vector<CallBack> callbackList{};
+};
+
+#endif //TFG_EVENTSTATE_H
